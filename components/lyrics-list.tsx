91ba04efb93db0b370fd002ba65e8d64f4b6ea56
NriotHrreion <nriot233@outlook.com>
2025-05-03 09:18:19 +0800

    feat: Use lyrics list component to list lyrics in the artist page

---
+++ components/lyrics-list.tsx
@@ -0,0 +1,24 @@
+import { Cards, Card } from 'fumadocs-ui/components/card';
+import { source } from '@/lib/source';
+
+interface LyricsListProps {
+    artist: string
+}
+
+export function LyricsList({ artist }: LyricsListProps) {
+    return (
+        <Cards>
+            {
+                source.getPages().map((page, index) => (
+                    page.url.startsWith(`/docs/${artist}/`)
+                    ? (
+                        <Card key={index} title={page.data.title} href={page.url}>
+                            {page.data.description}
+                        </Card>
+                    )
+                    : null
+                ))
+            }
+        </Cards>
+    );
+}
